d3c4530ca0e256e104c6e36aae17c54c19847fab
Tomas Aparicio <tomas@aparicio.me>
2014-09-02 01:45:26 +0200

    feat: add parser support

--- uri.js
+++ uri.js
@@ -14,7 +14,59 @@
   var VERSION = '0.1.0'
   var REGEX = /^(?:([^:\/?#]+):)?(?:\/\/((?:([^\/?#@]*)@)?([^\/?#:]*)(?:\:(\d*))?))?([^?#]*)(?:\?([^#]*))?(?:#((?:.|\n)*))?/i
 
-  function uri() {}
+  function uriParts(parts) {
+    var auth = (parts[3] || '').split(':')
+    return {
+      uri: parts[0],
+      protocol: parts[1],
+      host: parts[2].replace(/(.*\@)/, ''),
+      auth: parts[3],
+      user: auth[0],
+      password: auth[1],
+      hostname: parts[4],
+      port: parts[5],
+      path: parts[6],
+      query: parts[7],
+      fragment: parts[8]
+    }
+  }
+
+  function uriParser(uri) {
+    var parts = (uri || '').match(REGEX)
+    return uriParts(parts || [])
+  }
+
+  function URI(uri) {
+    this.parts = {}
+  }
+
+  URI.prototype.host = function (host) {
+    this.parts.host = host
+    return this
+  }
+
+  URI.prototype.auth = function (auth) {
+    this.parts.auth = auth
+    return this
+  }
+
+  URI.prototype.user = function (user) {
+    this.parts.user = user
+    return this
+  }
+
+  URI.prototype.build = URI.prototype.get = function () {
+    return this.parts
+  }
+
+  function uri(uri) {
+    if (uri) {
+      return uriParser(uri)
+    } else {
+      return new URI()
+    }
+  }
+
   uri.VERSION = VERSION
 
   exports.uri = uri
