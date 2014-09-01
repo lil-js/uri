7ca6f56958255296c6f88edbee0ed04763a0c538
Tomas Aparicio <tomas@aparicio.me>
2014-09-01 07:21:30 +0200

    feat: add UMD support

--- uri.js
+++ uri.js
@@ -1,17 +1,21 @@
 /*! lil-uri - v0.1 - MIT License - https://github.com/lil-js/uri */
-(function (global) {
-  var VERSION = '0.1.0'
-  var lil = global.lil || {}
-  var REGEX = /^(?:([^:\/?#]+):)?(?:\/\/((?:([^\/?#@]*)@)?([^\/?#:]*)(?:\:(\d*))?))?([^?#]*)(?:\?([^#]*))?(?:#((?:.|\n)*))?/i
-  
-  function uri() {}
-
-  type.VERSION = VERSION
-
-  lil.uri = uri
+(function (root, factory) {
   if (typeof define === 'function' && define.amd) {
-    define(function () { return type })
+    define(['exports'], factory)
+  } else if (typeof exports === 'object') {
+    factory(exports)
+    if (typeof module === 'object' && module !== null) {
+      module.exports = exports = exports.uri
+    }
   } else {
-    global.lil = lil
+    factory((root.lil = root.lil || {}))
   }
-}(this))
+}(this, function (exports) {
+  var VERSION = '0.1.0'
+  var REGEX = /^(?:([^:\/?#]+):)?(?:\/\/((?:([^\/?#@]*)@)?([^\/?#:]*)(?:\:(\d*))?))?([^?#]*)(?:\?([^#]*))?(?:#((?:.|\n)*))?/i
+
+  function uri() {}
+  uri.VERSION = VERSION
+
+  exports.uri = uri
+}))
